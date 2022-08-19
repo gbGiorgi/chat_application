bbafda89a22059fa989dc519056655e5aa6be6fb
= <=>
2022-08-19 09:44:19 +0400

    refresh scroll and input

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    /** on start */
+    connect() {
+        console.log("Connected");
+        const messages = document.getElementById("messages");
+        messages.addEventListener("DOMNodeInserted", this.resetScroll);
+        this.resetScroll(messages);
+    }
+    /** On stop */
+    disconnect() {
+        console.log("Disconnected")
+    }
+
+    /** Custom function */
+    resetScroll() {
+        messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+}
