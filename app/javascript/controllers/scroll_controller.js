ae7eac4306cab32f9957047f97943bb1269a89d0
= <=>
2022-08-24 17:06:48 +0400

    infinite scroll

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+    initialize() {
+        this.resetScrollWithoutThereshold(messages);
+    }
     /** on start */
     connect() {
         console.log("Connected");
         const messages = document.getElementById("messages");
         messages.addEventListener("DOMNodeInserted", this.resetScroll);
-        this.resetScroll(messages);
     }
     /** On stop */
     disconnect() {
@@ -15,6 +17,14 @@ export default class extends Controller {
 
     /** Custom function */
     resetScroll() {
+        const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
+        const upperScrollThreshold = bottomOfScroll - 500;
+        // Scroll down if we're not within the threshold
+        if (messages.scrollTop > upperScrollThreshold) {
+            messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+        }
+    }
+    resetScrollWithoutThereshold(messages) {
         messages.scrollTop = messages.scrollHeight - messages.clientHeight;
     }
 }
